2036f89b5b1e0d70dfdfc41df66342bdb1d58da7
Victor Marinho <victorcmarinho@gmail.com>
2023-07-24 00:40:36 -0300

    Skills

--- app/components/Main/index.tsx
+++ app/components/Main/index.tsx
@@ -1,11 +1,13 @@
 import MOF from "@/app/Drivers";
 import Home from "../Home";
 import About from "../About";
+import Skills from "../Skills";
 
 const Main = MOF<HTMLElement>((_, ref) => (
   <main ref={ref} className="flex flex-col gap-14">
     <Home />
     <About />
+    <Skills />
   </main>
 ));
 
