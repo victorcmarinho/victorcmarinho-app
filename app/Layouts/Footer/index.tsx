e2adc1bf2a27f3eac57326ad6487bd2ded50fbd3
Victor Marinho <victorcmarinho@gmail.com>
2023-07-25 02:04:56 -0300

    remove console.log

--- app/Layouts/Footer/index.tsx
+++ app/Layouts/Footer/index.tsx
@@ -3,14 +3,12 @@ import { UserDTO } from "../../DTOs/User.dto";
 import fetcher from "../../Services/fetcher";
 
 const Footer = async () => {
-  const { name, ...response } = await fetcher<UserDTO>(
+  const { name } = await fetcher<UserDTO>(
     "https://api.github.com/users/victorcmarinho",
     {
       next: { revalidate: 36 * 100000 },
     }
   );
-
-  console.log(response);
 
   return (
     <footer className="bg-secondary text-white text-center font-default py-8 px-0 mt-16 flex flex-col justify-center items-center gap-4 min-h-[200px] ">
